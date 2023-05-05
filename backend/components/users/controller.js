8fb49c76b3df6473e8e6483daafeef10f679436a
German Rangel <gnrg_74@hotmail.com>
2023-05-05 15:03:00 -0600

    password Encrypt

--- backend/components/users/controller.js
+++ backend/components/users/controller.js
@@ -1,54 +1,67 @@
 const User = require('./model');
 const response = require('../../utils/response');
 
-const createUser =  (req, res) => {
-    //validation
-    if (!req.body.name || !req.body.surname || !req.body.nickname || !req.body.password) {
-      response.error(req, res, 'missing data', 440, 'fill remaining data');
-      return;
+
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+
+const createUser = async (req, res) => {
+  //validation
+  if (!req.body.name || !req.body.surname || !req.body.nickname || !req.body.password) {
+    response.error(req, res, 'missing data', 440, 'fill remaining data');
+    return;
+  }
+
+  const passEnc = req.body.password;
+
+  const passCrypt = await bcrypt.hash(passEnc, saltRounds);
+
+
+  const user = new User({
+    name: req.body.name,
+    surname: req.body.surname,
+    nickname: req.body.nickname,
+    password: passCrypt,
+    email: req.body.email,
+    position: req.body.position,
+    image: req.file.originalname
+  });
+
+
+
+  User.find({
+    $or: [
+      { email: user.email.toLocaleLowerCase() },
+      { nickname: user.nickname.toLocaleLowerCase() }
+    ]
+  }).then((data) => {
+
+    if (data && data.length >= 1) {
+      response.succes(req, res, 'user exist', 201);
     }
-const user = new User({
-      name: req.body.name,
-      surname: req.body.surname,
-      nickname: req.body.nickname,
-      password: req.body.password,
-      email: req.body.email,
-      position: req.body.position,
-      image: req.file.originalname
-    });
+    else {
+        user.save(user)
+        .then(data => {
+          response.succes(req, res, data, 201);
+        })
+        .catch(err => {
+          response.error(req, res, 'Internal error', 500, err)
+        });
+    }
 
-    
-    User.find({$or:[
-      {email: user.email.toLocaleLowerCase()},
-      {nickname: user.nickname.toLocaleLowerCase()}
-    ]}).then((data)=>{
+  }).catch((error) => {
 
-      if(data && data.length >= 1){
-        response.succes(req,res,'user exist',201);
-      }
-      else{
-         //console.log(req.file);
-         user.save(user)
-         .then(data=>{
-           response.succes(req,res,data,201);
-         })
-         .catch(err => {
-           response.error(req,res,'Internal error',500, err)
-         });
-      }
-      
-    }).catch((error)=>{
-      
-      if(error){
-        return  response.error(req, res, 'Internal Error', 500, error);
-      }
-    })
-   
-  }
-  
-  
-  
-  module.exports = {
-    createUser,
-  
-  }
+    if (error) {
+      return response.error(req, res, 'Internal Error', 500, error);
+    }
+  })
+
+}
+
+
+
+module.exports = {
+  createUser,
+
+}
